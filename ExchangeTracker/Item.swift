924ca7bc698343d6a92e6ac13f423531f9ffe7b6
kamran.namazov <kamranamazov@yahoo.com>
2025-03-31 18:14:32 +0400

    Initial Commit

---
+++ ExchangeTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ExchangeTracker
+//
+//  Created by Kamran on 31.03.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
